3f11d1b13238cd78b93d25dfbdf25787c0437984
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-12-12 15:06:51 +0000

    Enhance QR code generation interface with input field validation and clear functionality

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -17,9 +17,42 @@ function generateQRCode(event) {
         qrImage.src = `data:image/png;base64,${data.qr_code_data}`;
         qrImage.style.display = "block";
         document.getElementById("qrBox").style.display = "flex";
+         const generateButton = form.querySelector("button[type='submit']");
+         generateButton.style.display = "none";
+
+         const inputField = document.getElementById("input");
+         inputField.disabled = true;
+         inputField.classList.add("bg-gray-100", "cursor-not-allowed");
       } else {
         alert("Error: " + JSON.stringify(data.errors));
       }
     })
     .catch((error) => console.error("Error:", error));
+}
+
+
+function clear() {
+  const form = document.getElementById("qrForm");
+  const inputField = document.getElementById("input");
+  const generateButton = form.querySelector("button[type='submit']");
+  const qrBox = document.getElementById("qrBox");
+  const qrImage = document.getElementById("qrCodeImage");
+  
+
+  // Clear the QR code image
+  qrImage.src = "";
+  qrImage.style.display = "none";
+
+  // Hide the QR code box
+  qrBox.style.display = "none";
+
+  // Re-enable the input field
+  inputField.disabled = false;
+  inputField.classList.remove("bg-gray-100", "cursor-not-allowed");
+
+  // Show the Generate button
+  generateButton.style.display = "block";
+
+  // Clear the input field value (optional)
+  inputField.value = "";
 }
